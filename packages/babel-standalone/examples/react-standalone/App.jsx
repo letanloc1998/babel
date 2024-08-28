3cf8288a374bad20f68bca3b4938e2383b406f53
Lê Tấn Lộc <26348718+letanloc1998@users.noreply.github.com>
2024-08-28 21:27:41 +0700

    Correct using babel - must fix module name in transform-modules-umd

--- packages/babel-standalone/examples/react-standalone/App.jsx
+++ packages/babel-standalone/examples/react-standalone/App.jsx
@@ -1,12 +1,12 @@
 // Copyright (c) 2024 Le Tan Loc. All rights reserved
 
-import A from "./components/A";
+// import A from "./components/A";
 
 const App = () => {
   return (
     <>
       <h1>App</h1>
-      <A />
+      {/* <A /> */}
     </>
   );
 };
