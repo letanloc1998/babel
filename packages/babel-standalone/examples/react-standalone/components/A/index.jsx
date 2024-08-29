0cfa791789161c8473c5569e429d6698c3317b35
Lê Tấn Lộc <26348718+letanloc1998@users.noreply.github.com>
2024-08-29 16:27:11 +0700

    Temporary fix relative import

--- packages/babel-standalone/examples/react-standalone/components/A/index.jsx
+++ packages/babel-standalone/examples/react-standalone/components/A/index.jsx
@@ -1,7 +1,10 @@
 // Copyright (c) 2024 Le Tan Loc. All rights reserved
 
-import B from "/components/B";
-import C from "/components/C";
+// import B from "/components/B";
+// import C from "/components/C";
+
+import B from "../B";
+import C from "../C";
 
 const A = () => (
   <>
