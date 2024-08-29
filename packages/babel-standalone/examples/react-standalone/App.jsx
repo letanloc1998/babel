92142f733409fd8f4273b860547ea2ec15f27bdf
Lê Tấn Lộc <26348718+letanloc1998@users.noreply.github.com>
2024-08-29 14:23:35 +0700

    Temporary React lazy load

--- packages/babel-standalone/examples/react-standalone/App.jsx
+++ packages/babel-standalone/examples/react-standalone/App.jsx
@@ -1,10 +1,86 @@
 // Copyright (c) 2024 Le Tan Loc. All rights reserved
 
-// import A from "./components/A";
+// import A from "/components/A";
 
-const { useState, useEffect } = React;
+const { useState, useEffect, lazy } = React;
 
 // import Lazy from "/Lazy";
+
+// const ReactLazy = lazy(
+// 	() =>
+// 		new Promise((resolve) => {
+// 			setTimeout(() => {
+// 				resolve({
+// 					default: () => <h1>ReactLazy</h1>,
+// 				});
+// 			}, 2000);
+// 		})
+// );
+
+// const ReactLazy = lazy(
+//   () =>
+//     new Promise(async resolve => {
+
+//       const loadModule = async url =>
+//         await fetch(url)
+//           .then(response => response.text())
+//           .then(
+//             text =>
+//               Babel.transform(text, {
+//                 // filename: normalizedFilename(url), // global.unknown if no filename
+//                 // filename: new URL(url, window.location.href).href, // only apply baseURL for relative or absolute path
+//                 filename: url,
+//                 // sourceType: "module",
+//                 presets: [[Babel.availablePresets["react"]]],
+//                 plugins: [
+//                   [
+//                     Babel.availablePlugins["transform-modules-umd"],
+//                     { exactGlobals: true },
+//                   ],
+//                 ],
+//               }).code
+//           )
+//           .then(code => eval(code));
+
+//       await loadModule("/ReactLazy.jsx");
+
+//       resolve({
+//         default: () => window.ReactLazy.default,
+//       });
+//     })
+// );
+
+// const ReactLazy = lazy(() => {
+//   return Promise.resolve({ default: window.ReactLazy.default });
+// });
+
+const loadComponent = async () => {
+  const url = "/ReactLazy.jsx";
+  await fetch(url)
+    .then(response => response.text())
+    .then(
+      text =>
+        Babel.transform(text, {
+          // filename: normalizedFilename(url), // global.unknown if no filename
+          // filename: new URL(url, window.location.href).href, // only apply baseURL for relative or absolute path
+          filename: url,
+          // sourceType: "module",
+          presets: [[Babel.availablePresets["react"]]],
+          plugins: [
+            [
+              Babel.availablePlugins["transform-modules-umd"],
+              { exactGlobals: true },
+            ],
+          ],
+        }).code
+    )
+    .then(code => eval(code));
+
+  return { default: window.ReactLazy.default };
+}
+
+const ReactLazy = lazy(loadComponent);
+
 
 const App = () => {
   const [component, setComponent] = useState(null);
@@ -33,7 +109,6 @@ const App = () => {
         .then(code => {
           // console.log("code", code);
           // return `data:text/javascript;base64,${btoa(code)}`;
-          
 
           // var moduleData = "export function hello() { alert('hello'); };";
           // var b64moduleData = "data:text/javascript;base64," + btoa(moduleData);
@@ -43,19 +118,25 @@ const App = () => {
           eval(code);
           setComponent(window.Lazy.default);
           setModule(window.Lazy);
-        })
-        // .then(async jsModule => {
-        //   const m = await import(jsModule);
-        //   console.log("m", m);
+        });
+    // .then(async jsModule => {
+    //   const m = await import(jsModule);
+    //   console.log("m", m);
 
-        //   import(jsModule).then(module => {
-        //     console.log("module", module);
-        //     console.log("module.default", module.default);
-        //     setComponent(module.default);
-        //   });
-        // });
+    //   import(jsModule).then(module => {
+    //     console.log("module", module);
+    //     console.log("module.default", module.default);
+    //     setComponent(module.default);
+    //   });
+    // });
 
     loadModule("/Lazy.jsx");
+
+    // const tempLoad = async () => {
+    //   loadComponent().then(r => console.log(r));
+    // }
+
+    // tempLoad();
   }, []);
 
   return (
@@ -63,6 +144,7 @@ const App = () => {
       <h1>App</h1>
       {/* <A /> */}
       {component}
+      <ReactLazy />
     </>
   );
 };
---
+++ packages/babel-standalone/examples/react-standalone/ReactLazy.jsx
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Le Tan Loc. All rights reserved
+
+const ReactLazy = () => (
+    <>
+      <h1>ReactLazy</h1>
+    </>
+  );
+  
+  export default ReactLazy;
+  
